2ec26697bc2e0934d401aa98beaad8ad5efb8cad
sebasr77 <59916732+sebasr77@users.noreply.github.com>
2020-11-08 17:10:46 +0100

    empty project

--- src/main/java/pl/sebasr/ksb2/Ksb2Application.java
+++ src/main/java/pl/sebasr/ksb2/Ksb2Application.java
@@ -1,13 +1,13 @@
-package com.example.httpclient;
+package pl.sebasr.ksb2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HttpClientApplication {
+public class Ksb2Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(HttpClientApplication.class, args);
+        SpringApplication.run(Ksb2Application.class, args);
     }
 
 }
